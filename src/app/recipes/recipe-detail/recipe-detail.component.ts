f1525048730329f0e2165a9a961eeb77b6f24d2c
d.rabchenyuk <d.rabchenyuk@gmail.com>
2019-05-03 18:43:45 +0300

    implemented adding ingredients to shopping list

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,8 @@
+import { RecipeService } from './../recipe.service';
+import { ShoppingListService } from './../../shopping-list/shopping-list.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { Ingredient } from 'src/app/shared/ingredient.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,9 +13,16 @@ export class RecipeDetailComponent implements OnInit {
   @Input() recipe: Recipe;
   isOpen = false;
 
-  constructor() { }
+  constructor(private slService: ShoppingListService,
+              private recipeService: RecipeService) { }
 
   ngOnInit() {
+  }
+
+  toShoppingList() {
+    // this.recipe.ingredients.forEach((ingredient: Ingredient) => this.slService.addIngredient(ingredient));
+    // or
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
   }
 
   openModal() {
